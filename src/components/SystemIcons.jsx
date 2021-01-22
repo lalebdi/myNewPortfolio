69b9b794b9ea503f60e24bbc3bbc52147f1900d5
lalebdi <loai.alebdi@gmail.com>
2021-01-21 21:48:32 -0500

    chnaged the icons

--- src/components/SystemIcons.jsx
+++ src/components/SystemIcons.jsx
@@ -10,9 +10,9 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faTwitter,
-  faFacebookF,
+  faCreativeCommonsShare,
   faLinkedinIn,
-  faGithubAlt,
+  faGithub,
   faMediumM,
 } from "@fortawesome/free-brands-svg-icons";
 
@@ -20,13 +20,13 @@ export const PhoneIcon = makeFAIcon(faPhone);
 export const EnvelopIcon = makeFAIcon(faEnvelope);
 export const PlusIcon = makeFAIcon(faPlus);
 export const BarsIcon = makeFAIcon(faBars);
-export const GithubIcon = makeFAIcon(faGithubAlt);
+export const GithubIcon = makeFAIcon(faGithub);
 export const MediumIcon = makeFAIcon(faMediumM);
 export const CloseIcon = makeFAIcon(faTimes);
 export const LanguageIcon = makeFAIcon(faGlobe);
 
 export const TwitterIcon = makeFAIcon(faTwitter);
-export const FacebookIcon = makeFAIcon(faFacebookF);
+export const FacebookIcon = makeFAIcon(faCreativeCommonsShare);
 export const LinkedinIcon = makeFAIcon(faLinkedinIn);
 
 export * from "config/CustomIcons";
