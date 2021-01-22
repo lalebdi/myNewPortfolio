b74bd75c045c4f6ec9579e46ff5361d783d024c3
lalebdi <loai.alebdi@gmail.com>
2021-01-21 21:07:42 -0500

    removed the clients component

--- src/views/Sections/index.js
+++ src/views/Sections/index.js
@@ -3,6 +3,6 @@ export { default as Contact } from "./Contact";
 export { default as Portfolio } from "./Portfolio";
 export { default as Services } from "./Services";
 // export { default as Team } from "./Team";
-export { default as Clients } from "./Clients";
+// export { default as Clients } from "./Clients";
 
 
